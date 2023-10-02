0007dd8ed507d9ca53ce11368869a91d6df5cada
faseian <nab4nkert@outlook.com>
2023-10-02 08:29:57 -0700

    update

--- dicegame/simongame/simonjava.js
+++ dicegame/simongame/simonjava.js
@@ -1,31 +1,65 @@
+var seqPattern = [];
+var playerPattern = [];
+var level = 1;
+
+
 $(".btn").click(function() { //adds animation
+
     var button = $(this); 
     button.addClass("pressed");
+    var colorBtn = this.id;
+    playerPattern.push(colorBtn);
     setTimeout(function() {
         button.removeClass("pressed");
     }, 100);
+    playerPattern.forEach((element) => console.log("Player: " + element));
 })
 
-$(document).click(function() {
-    sequence();
+function ansCheck(pPattern, cPattern) {
+    if (pPattern == cPattern) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+$(document).keydown(function(e) {
+    console.log(e.key)
+    if (e.key ==' '){
+        gameStart();
+        nextSequence();
+    }
 })
 
-function sequence() {
+function gameStart() {
+    level = 0;
+    $("h1").text("Level " + level);
+    playerPattern = [];
+    seqPattern = [];
+}
+
+function nextSequence() {
     var block = Math.floor(Math.random() * 4) + 1;
-
     switch(block) {
         case 1:
             $("#green").fadeOut(100).fadeIn(100);
+            seqPattern.push("green");
             break;
         case 2:
             $("#red").fadeOut(100).fadeIn(100);
+            seqPattern.push("red");
             break;
         case 3:
             $("#yellow").fadeOut(100).fadeIn(100);
+            seqPattern.push("yellow");
             break;
         case 4:
             $("#blue").fadeOut(100).fadeIn(100);
+            seqPattern.push("blue");
             break;
 
     }
+    level++;
+    $("h1").text("Level " + level);
+    seqPattern.forEach((element) => console.log("CPU: " + element));
 }
